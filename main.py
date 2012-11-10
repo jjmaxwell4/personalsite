e05a1a64bdcb791da9efd4640932ce710280c18c
JJ Maxwell <jjmaxwell4@gmail.com>
2012-11-10 12:44:23 -0500

    Added Blog Snipit for Front Page of Blog

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import os
 import re
 
@@ -88,6 +72,12 @@ class Post(db.Model):
 
     def render(self):
         self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
+
+    def render_snipit(self):
+        self.content = self.content.replace('\n', '<br>')
+        snipit = ' '.join(self.content.split(' ')[:100])+" ..."
+        self._render_text = snipit
         return render_str("post.html", p = self)
 
 class BlogFront(BlogHandler):
